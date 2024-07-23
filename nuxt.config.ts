32532a13adc52b29f19960407a2daed80ba4699f
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-07-23 13:34:34 +0200

    :heavy_plus_sign: Add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "shadcn-nuxt",
     "@nuxtjs/eslint-module"
   ]
 });

a0cb515c34f903a47cd2737f9b8c18db542ad6b8
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-07-24 11:54:31 +0200

    :rocket: Handle error and rename

--- server/api/auth/register.ts
+++
@@ -1,7 +0,0 @@
-import { defineEventHandler, readBody } from "#imports";
-import {register} from "~/server/services/auth";
-import {IUser, IUserCreateBody} from "~/types/user";
-
-export default defineEventHandler(async (event): Promise<IUser> => {
-  return await register(event, await readBody<IUserCreateBody>(event));
-});

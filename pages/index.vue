152337757c62d9767defefde64fb5c3e488da319
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-07-26 02:44:34 +0200

    :rocket: Use user provider

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 import {Button} from "~/components/ui/button";
 import {type IUser} from "~/types/user";
 import {useState} from "#imports";
-import {logoutUser} from "~/composables/auth";
+import {UserProvider} from "~/components/shared/user";
 
 const user = useState<IUser>("user");
 </script>
@@ -10,18 +10,21 @@ const user = useState<IUser>("user");
 <template>
   <div class="flex flex-col items-start">
     <h1>{{ user ? `Welcome ${user.username}` : "Home" }}</h1>
-    <Button variant="destructive" @click="logoutUser" v-if="user">Logout</Button>
-    <div class="flex gap-2" v-else>
-      <Button variant="secondary" as-child>
-        <NuxtLink to="/portal/auth/login">
-          Login
-        </NuxtLink>
-      </Button>
-      <Button as-child>
-        <NuxtLink to="/portal/auth/register">
-          Register
-        </NuxtLink>
-      </Button>
-    </div>
+    <UserProvider>
+      <template #fallback>
+        <div class="flex gap-2" v-if="!user">
+          <Button variant="secondary" as-child>
+            <NuxtLink to="/portal/auth/login">
+              Login
+            </NuxtLink>
+          </Button>
+          <Button as-child>
+            <NuxtLink to="/portal/auth/register">
+              Register
+            </NuxtLink>
+          </Button>
+        </div>
+      </template>
+    </UserProvider>
   </div>
 </template>
